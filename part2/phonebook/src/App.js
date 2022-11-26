097101d9f882096396a0c3bd2ab012e29d2d933f
Gabriel Renard <gabriel.renard@protonmail.com>
2022-11-26 13:07:36 +0100

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const nameChangeHandler = e => {
     setNewName(e.target.value);
@@ -34,28 +40,42 @@ const App = () => {
     setNewNumber("");
   };
 
+  const filterNameHandler = e => {
+    setSearch(e.target.value);
+  };
+
+  const phoneBookList = persons
+    .filter(person => {
+      return person.name.toLowerCase().includes(search.toLowerCase());
+    })
+    .map(person => (
+      <li key={person.id}>
+        {person.name} {person.number}
+      </li>
+    ));
+
   return (
     <div>
+      <div>
+        <label htmlFor="filter">Filter</label>
+        <input type="text" id="filter" onChange={filterNameHandler} />
+      </div>
       <h2>Phonebook</h2>
       <form onSubmit={formSubmitHandler}>
         <div>
-          name: <input onChange={nameChangeHandler} value={newName} />
+          <label htmlFor="name">Name</label>
+          <input id="name" onChange={nameChangeHandler} value={newName} />
         </div>
         <div>
-          number: <input onChange={numberChangeHandler} value={newNumber} />
+          <label htmlFor="number">Number</label>
+          <input id="number" onChange={numberChangeHandler} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => (
-          <li key={person.id}>
-            {person.name} {person.number}
-          </li>
-        ))}
-      </ul>
+      <ul>{phoneBookList}</ul>
     </div>
   );
 };
